cfd23c05c5e929bed2ff6e9829b52fcefb88af09
Xuan-Zhang Gong <gongxuanzhangmelt@gmail.com>
2024-09-23 10:01:03 +0800

    add re jar task

--- buildSrc/src/main/kotlin/tech/medivh/build/JarAgainPlugin.kt
+++ buildSrc/src/main/kotlin/tech/medivh/build/JarAgainPlugin.kt
@@ -4,6 +4,8 @@ import java.io.File
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.Exec
+import org.gradle.internal.impldep.org.bouncycastle.asn1.x500.style.RFC4519Style.o
+import sun.tools.jar.resources.jar
 
 
 /**
@@ -14,40 +16,35 @@ class JarAgainPlugin : Plugin<Project> {
     override fun apply(project: Project) {
 
         project.afterEvaluate {
-            val core = project.rootProject.project(":medivh-core")
-            val jar = core.tasks.named("jar").get()
-
-            val agentFrom = core.layout.buildDirectory.dir("libs").get().file(agentJarName(project)).asFile
-            
-            core.tasks.register("doJar", Exec::class.java) {
-                if (!agentFrom.exists()) {
-                    it.workingDir(project.rootDir)
-
-                    val osName = System.getProperty("os.name").lowercase()
-                    val gradlew = if (osName.contains("windows")) {
-                        "gradlew.bat"
-                    } else {
-                        "./gradlew"
-                    }
-                    it.commandLine(gradlew, ":medivh-core:jar")
-                }
-               
-                it.doLast {
-                    if (agentFrom.exists()) {
+            val onlyJar: String? = project.findProperty("onlyJar") as String? ?: "false"
+            if (!onlyJar.toBoolean()) {
+                registerDojar(project)
+                val core = project.rootProject.project(":medivh-core")
+                val jar = core.tasks.named("jar") {
+                    it.dependsOn("doJar")
+                    it.doLast {
+                        val agentFrom = core.layout.buildDirectory.dir("libs").get().file(agentJarName(project)).asFile
                         val resourceDir = File(core.projectDir, "src/main/resources")
                         val target = File(resourceDir, "medivh-agent-${project.version}.jar")
                         agentFrom.copyTo(target, true)
                     }
                 }
             }
-
-            core.tasks.named("jar") {
-                if(!agentFrom.exists()) {
-                    it.dependsOn("doJar")
-                }
-            }
         }
+    }
 
+    private fun registerDojar(project: Project) {
+        val core = project.rootProject.project(":medivh-core")
+        core.tasks.register("doJar", Exec::class.java) {
+            it.workingDir(project.rootProject.rootDir)
+            val osName = System.getProperty("os.name").lowercase()
+            val gradlew = if (osName.contains("windows")) {
+                "gradlew.bat"
+            } else {
+                "./gradlew"
+            }
+            it.commandLine(gradlew, ":medivh-core:jar", "-PonlyJar=true")
+        }
     }
 
     private fun agentJarName(project: Project): String {
